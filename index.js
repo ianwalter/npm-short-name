6af42bbca2e27cc01d0a0ce623e45e4d9cfc7a3e
Ian Walter <public@iankwalter.com>
2019-01-15 12:24:49 -0500

    Fixing module value

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import camelCase from 'camelcase'
 
-export default function npmShortName (name = '') {
-  const parts = name.split('/')
+export default function npmShortName (name) {
+  const parts = (name || '').split('/')
   return parts.length ? camelCase(parts[parts.length - 1]) : ''
 }
